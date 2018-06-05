5b57db06334ddd91b554fc0f1a7756f03d6b0f7c
Chandler <ChandlerAHayes@gmail.com>
2018-06-05 12:53:06 -0700

    Removed navigation drawer in LoginActivity

--- App/app/src/main/java/chayes/guzzle/Account/LoginActivity.java
+++ App/app/src/main/java/chayes/guzzle/Account/LoginActivity.java
@@ -2,16 +2,11 @@ package chayes.guzzle.Account;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -21,11 +16,7 @@ import chayes.guzzle.MyJournal.MyJournalActivity;
 import chayes.guzzle.R;
 
 public class LoginActivity extends AppCompatActivity {
-    private DrawerLayout drawerLayout;
     private FirebaseAuth authentication;
-
-    // Constants
-    private static final String TAG = "LoginActivty";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,26 +29,6 @@ public class LoginActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
-
-        //-------- Navigation Drawer
-        drawerLayout = findViewById(R.id.drawer_layout);
-        NavigationView navView = findViewById(R.id.nav_view);
-        navView.setNavigationItemSelectedListener(new NavigationView
-                .OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                // highlight selected item
-                item.setCheckable(true);
-
-                // close drawer
-                drawerLayout.closeDrawers();
-
-                //TODO: open corresponding page
-                // navigationItemSelected(item)
-
-                return true;
-            }
-        });
 
         // let the user log into their account
         login();
@@ -83,24 +54,5 @@ public class LoginActivity extends AppCompatActivity {
             startActivity(new Intent(LoginActivity.this, MyJournalActivity.class));
         }
 
-    }
-
-    //-------- Menu & Navigation Drawer Functions
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                drawerLayout.openDrawer(GravityCompat.START);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    private void navigationItemSelected(MenuItem item){
-        FragmentController controller = new FragmentController(getSupportFragmentManager());
-        switch (item.getItemId()){
-            case R.id.home:
-                break;
-        }
     }
 }
